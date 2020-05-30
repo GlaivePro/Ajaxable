5e1d318df5dde6ecf55bf230ccf4f17ae17d66ac
Džuris <juris@glaive.pro>
2020-05-30 04:06:31 +0300

    Add testing migration

---
+++ tests/migrations/0000_00_00_000000_create_models_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateModelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('models', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('col1');
+            $table->string('col2S');
+            $table->string('col3');
+            $table->string('col4');
+            $table->string('col5');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('models');
+    }
+}
