3ecab6cf6c0361c04b609e9a5bae0db3a223e603
Džuris <juris@glaive.pro>
2020-05-30 04:10:26 +0300

    Add HTTP verb routes

--- src/routes.php
+++ src/routes.php
@@ -5,7 +5,7 @@ Route::group(['prefix' => 'ajaxable', 'middleware' => 'web'], function () {
 	Route::post('create', 'GlaivePro\Ajaxable\Controller@create')->name('ajaxable.create');
 	Route::get('retrieve', 'GlaivePro\Ajaxable\Controller@retrieve')->name('ajaxable.retrieve');
 	Route::match(['post', 'patch'], 'update', 'GlaivePro\Ajaxable\Controller@update')->name('ajaxable.update');
-	Route::match(['post', 'delete'], 'delete', 'GlaivePro\Ajaxable\Controller@delete')->name('ajaxable.delete');
+	Route::match(['post', 'delete'], 'delete', 'GlaivePro\Ajaxable\Controller@delete')->name('ajaxable.remove');
 	
 	// Extras
 	Route::match(['post', 'put'], 'update-or-create', 'GlaivePro\Ajaxable\Controller@updateOrCreate')->name('ajaxable.updateOrCreate');
@@ -16,4 +16,11 @@ Route::group(['prefix' => 'ajaxable', 'middleware' => 'web'], function () {
 	Route::post('add-media', 'GlaivePro\Ajaxable\Controller@addMedia')->name('ajaxable.addMedia');
 	Route::get('get-media', 'GlaivePro\Ajaxable\Controller@getMedia')->name('ajaxable.getMedia');
 	Route::match(['post', 'delete'], 'delete-media', 'GlaivePro\Ajaxable\Controller@deleteMedia')->name('ajaxable.deleteMedia');
+
+	// Verbs
+	Route::post('/', 'GlaivePro\Ajaxable\Controller@create')->name('ajaxable.post');
+	Route::get('/', 'GlaivePro\Ajaxable\Controller@retrieve')->name('ajaxable.get');
+	Route::patch('/', 'GlaivePro\Ajaxable\Controller@update')->name('ajaxable.patch');
+	Route::delete('/', 'GlaivePro\Ajaxable\Controller@delete')->name('ajaxable.delete');
+	Route::put('/', 'GlaivePro\Ajaxable\Controller@updateOrCreate')->name('ajaxable.put');
 });
